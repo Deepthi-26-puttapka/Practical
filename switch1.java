677033fb0e67499e3126dada8dc024f32de52c65
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-04 10:12:15 +0530

    Add files via upload

---
+++ switch1.java
@@ -0,0 +1,36 @@
+package javaprograms;
+
+public class switch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day=4;
+		switch(day){
+		case 1:
+			System.out.print("sunday");
+		break; 
+		case 2:
+			System.out.print("monday");
+		break; 
+		case 3:
+			System.out.print("tuesday");
+		break; 
+		case 4:
+			System.out.print("wedsday");
+		break; 
+		case 5:
+			System.out.print("thrusday");
+		break; 
+		case 6:
+			System.out.print("friday");
+			break; 
+		case 7:
+			System.out.print("strudayday");
+			break; 
+			
+		}
+	
+
+	}
+
+}
