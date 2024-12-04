10cced31b6d11badb130533b47faced3bd91f6f5
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-04 10:08:59 +0530

    Add files via upload

---
+++ bean1.java
@@ -0,0 +1,17 @@
+package javaprograms;
+
+public class bean1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=6;
+		System.out.println("addition=:"+(a+b));
+		System.out.println("multiplication=:"+(a*b));
+		System.out.println("subraction=:"+(a-b));
+		System.out.println("division=:"+(a/b));
+		System.out.println("modulas=:"+(a%b));
+
+
+	}
+
+}
