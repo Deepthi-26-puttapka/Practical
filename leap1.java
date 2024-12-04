7e7b2fae67ecb10d70c5e7a1f1de3f675199c791
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-04 09:59:24 +0530

    Add files via upload

---
+++ leap1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class leap1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+          System.out.println("enter a year");
+		int n=sc.nextInt();
+		if(n%4==0&&n%100!=0||n%400==0)
+		{
+			System.out.println("leap year");
+			
+		}
+		else
+		{
+			System.out.println("not a leap year ");
+		}
+		
+	}
+
+}
