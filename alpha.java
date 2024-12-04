fc27f914ca61aaa7c7f63a2cd6d91d4f2a5d2372
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-04 09:55:00 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class alpha {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter a character");
+		String c= sc.nextLine();
+                if(c=='a'|| c=='e'||c=='i'|| c=='o'||c=='u')
+{
+  System.out.println(" vowel");
+}
+ else
+{
+System.out.println(" consonents");
+}
+
+	
+
+	}
+
+}
