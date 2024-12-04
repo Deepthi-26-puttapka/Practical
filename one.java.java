3c1d8ca4be30c6037b7df8f638061be221bf86bf
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-04 09:59:48 +0530

    Add files via upload

---
+++ one.java.java
@@ -0,0 +1,12 @@
+public class first
+{
+public static void main(String arg[])
+{
+System.out.println("Deepthi");
+byte a=10;
+int b=20;
+int sum=a+b;
+System.out.println("sum of a and b="+sum);
+}
+}
+
