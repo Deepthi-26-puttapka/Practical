83ace826de8c39834641056f8c067caf0e2a8d45
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2024-12-12 16:09:43 +0530

    Add files via upload

---
+++ Patterns10.java
@@ -0,0 +1,22 @@
+package javaprograms;
+
+public class Patterns10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                // Print stars at the boundary, else print space
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
