f6771794a9c69799a6bea37961ad2cec6201325f
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2025-03-19 10:32:20 +0530

    Add files via upload

---
+++ Ascii_values.java
@@ -0,0 +1,17 @@
+package javaprograms.Arrays1;
+
+public class Ascii_values {
+
+	public static void main(String[] String)   
+	{  
+	int c1= 'a';
+	int c2= 'b';
+	System.out.println("The ASCII value of a is: "+c1);  
+	System.out.println("The ASCII value of b is: "+c2); 
+	
+	for(int i = 65; i <= 90; i++)  
+	{  
+	System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+	}  
+	}  
+	}
