194cda6e385ec3835af228379a8ad612de9cbbd9
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2025-03-20 14:46:02 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,20 @@
+package javaprograms.Arrays1;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number= 0;
+		if(number>0) {
+			System.out.print(" number is Positive");
+		}
+		else if(number<0) {
+			System.out.print(" number is Negative");
+		}
+		else {
+			System.out.print(" Given number is zero");
+		}
+	}
+
+}
