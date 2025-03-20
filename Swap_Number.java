564d35ce2d81e436cd8edbb343931f2092f779ae
Deepthi-26-puttapka <129882276+Deepthi-26-puttapka@users.noreply.github.com>
2025-03-20 14:15:45 +0530

    Add files via upload

---
+++ Swap_Number.java
@@ -0,0 +1,16 @@
+package javaprograms.Arrays1;
+
+public class Swap_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20;
+		System.out.println("Before swap a="+a+" b="+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("after swap a="+a+" b="+b);
+
+	}
+
+}
